b6aa5d78441aceff4882d3581d42af533e66b9ef
payasrelekar <relekarpayas@gmail.com>
2017-10-22 21:11:00 +0530

    Add keybindings for find and open function.
    Set <C-p> as default keybindings for finding a file through fzf and opening it through default editor.

--- key_bindings.fish
+++ key_bindings.fish
@@ -5,6 +5,7 @@ if test "$FZF_LEGACY_KEYBINDINGS" -eq 1
     bind \ct '__fzf_find_file'
     bind \cr '__fzf_reverse_isearch'
     bind \cx '__fzf_find_and_execute'
+    bind \cp '__fzf_find_and_open'
     bind \ec '__fzf_cd'
     bind \eC '__fzf_cd_with_hidden'
 
@@ -12,12 +13,14 @@ if test "$FZF_LEGACY_KEYBINDINGS" -eq 1
         bind -M insert \ct '__fzf_find_file'
         bind -M insert \cr '__fzf_reverse_isearch'
         bind -M insert \cx '__fzf_find_and_execute'
+        bind -M insert \cp '__fzf_find_and_open'
         bind -M insert \ec '__fzf_cd'
         bind -M insert \eC '__fzf_cd_with_hidden'
     end
 else
     bind \cf '__fzf_find_file'
     bind \cr '__fzf_reverse_isearch'
+    bind \cp '__fzf_find_and_open'
     bind \ex '__fzf_find_and_execute'
     bind \eo '__fzf_cd'
     bind \eO '__fzf_cd_with_hidden'
@@ -25,6 +28,7 @@ else
     if bind -M insert >/dev/null ^/dev/null
         bind -M insert \cf '__fzf_find_file'
         bind -M insert \cr '__fzf_reverse_isearch'
+        bind -M insert \cp '__fzf_find_and_open'
         bind -M insert \ex '__fzf_find_and_execute'
         bind -M insert \eo '__fzf_cd'
         bind -M insert \eO '__fzf_cd_with_hidden'
